830c66663ec583ec8ce37a0abae27c8caf4415bf
hayamizhin <max9173627>
2020-03-04 18:05:47 +0900

    [add]sign_in時にfolders_pathに遷移させる

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
-    # :sessions => 'users/sessions'
+    :sessions => 'users/sessions'
   } 
 
   root 'home#top'
