652953a3b9170ad8c0992e4f1c2a89e957688864
hayamizhin <max9173627>
2020-03-24 15:05:32 +0900

    [add]Rspecによるテスト(user_modelとfolder_model)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = build(:user)
+  end
 
-  context "ユーザー新規登録時のヴァリデーション機能" do
-    it "値が入っている場合登録できる" do
-      user = build(:user)
-      expect(user).to be_valid
-    end
+  it 'ユーザー登録が可能である' do
+    expect(build(:user)).to be_valid
+  end
 
-    # it 'nameが空では登録できないこと' do
-    #   user = build(:user, name: nil)
-    #   user.valid?
-    #   expect(user.errors[:name]).to include('が入力されていません。')
-    # end    
+  # it'nameが存在しないユーザーを許可しない' do
+  #   @user.name = nil
+  #   @user.valid?
+  #   expect(@user.errors).to be_added(:blank)
+  # end  
 
-    it 'emailが空では登録できないこと' do
-      user = build(:user, email: nil)
-      user.valid? #有効か？
-      # expect(user.errors[:email]).to include('入力してください')
-    end
+  # it 'nameが空白のユーザーを許可しない' do
+  #   @user.name = '   '
+  #   @user.valid?
+  #   expect(@user.errors).to be_added(:name, :blank)
+  # end
 
+  it 'emailが存在しないユーザーを許可しない' do
+    @user.email = nil
+    @user.valid?
+    expect(@user.errors).to be_added(:email, :blank)
   end  
+
+  # it 'nameが空では登録できないこと' do
+  #   user = build(:user, name: nil)
+  #   user.valid?
+  #   expect(user.errors[:name]).to include('が入力されていません。')
+  # end    
+
+  it 'emailが空では登録できないこと' do
+    user = build(:user, email: nil)
+    user.valid? #有効か？
+    # expect(user.errors[:email]).to include('入力してください')
+  end
 end
