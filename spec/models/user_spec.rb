020f70d7e4e4cfe3b9e8393a2f7b6501a0a543d0
hayamizhin <max9173627>
2020-03-24 16:12:46 +0900

    [add]rspecの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
+# 参考
+# nameを一意にする
+#nameの長さを調整
+# https://github.com/TadayoshiOtsuka/NateMate/blob/master/spec/models/user_spec.rb
+
 RSpec.describe User, type: :model do
   before do
     @user = build(:user)
@@ -38,4 +43,35 @@ RSpec.describe User, type: :model do
     user.valid? #有効か？
     # expect(user.errors[:email]).to include('入力してください')
   end
+
+  it 'passwordとpassword_confirmationの一致しないユーザーを許可しない' do
+    @user.password = 'password'
+    @user.password_confirmation = 'aaaaaaaa'
+    @user.valid?
+    expect(@user.errors).to be_added(:password_confirmation, :confirmation, attribute: 'パスワード')
+  end  
+
+  it 'passwordが6文字未満のユーザーを許可しない' do
+    @user.password = 'a' * 5
+    @user.valid?
+    expect(@user.errors).to be_added(:password, :too_short, count: 6)
+  end  
+
+  it 'passwordが６文字以上のユーザーを許可する' do
+    @user.password = @user.password_confirmation = 'a' * 6
+    expect(@user).to be_valid
+  end  
+
+  # it 'nameが一意でないユーザーを許可しない' do
+  #   user  = create(:user, name: 'sample')
+  #   user2 = build(:user, name: 'sample')
+  #   user2.valid?
+  #   expect(user2.errors).to be_added(:name, :taken, value: 'sample')
+  # end
+  it 'emailは全て小文字で保存される' do
+    @user.email = 'SAMPLE@SAMPLE.JP'
+    @user.save!
+    expect(@user.reload.email).to eq 'sample@sample.jp'
+  end  
+
 end
