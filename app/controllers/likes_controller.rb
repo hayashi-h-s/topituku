e480907a80381aaf56ba153b1496921822b48ae7
haya <haya@appurunoMacBook-puro.local>
2020-04-27 12:58:09 +0900

    [add]いいいね機能の追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,18 @@
 class LikesController < ApplicationController
 
   def create
-    # binding.pry
-    @like = current_user.likes.create(folder_id: params[:folder_id])
+    # @folder = Folder.find(params[folder_id])
+    @like = current_user.likes.build(folder_id: params[:folder_id])
+    @like.save
     # binding.pry
     redirect_back(fallback_location: root_path)
     # rdirect_to root_path
   end
 
   def destroy
+    # @folder = Folder.find(params[folder_id])
     @like = Like.find_by(folder_id: params[:folder_id], user_id: current_user.id)
+    # binding.pry
     @like.destroy
     redirect_back(fallback_location: root_path)
     # ridirect_to root_path
