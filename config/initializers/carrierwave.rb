21c63e8c040afcac651935e18d95d1db05467b49
hayashi-h-s <max9173267@gmail.com>
2020-05-03 04:51:51 +0000

    [add]EC2にデプロイ

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -30,16 +30,17 @@ end
 
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      # Amazon S3用の設定
+      # Amazon S3用の設定 #
       provider: 'AWS',
-      region: ENV['AWS_S3_REGION'],
-      aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
+      region: 'ap-northeast-1',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
     }
-    config.fog_directory     =  ENV['AWS_S3_BUCKET']
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+    config.fog_directory = 'topituku'
+    config.asset_host = 'https://topituku.s3.amazonaws.com' 
   end
 
   # 日本語ファイル名の設定
