3f590dfa97bb4bf58b312022b8b2773d09e76248
haya <haya@appurunoMacBook-puro.local>
2020-05-16 16:29:39 +0900

    [add]herokuにデプロイ

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -30,17 +30,16 @@ end
 
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      # Amazon S3用の設定 #
+      # Amazon S3用の設定
       provider: 'AWS',
-      region: 'ap-northeast-1',
-      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: ENV['AWS_S3_REGION'],
+      aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
     }
-    config.fog_directory = 'topituku'
-    config.asset_host = 'https://topituku.s3.amazonaws.com' 
+    config.fog_directory =  ENV['AWS_S3_BUCKET']
+    # config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   end
 
   # 日本語ファイル名の設定
