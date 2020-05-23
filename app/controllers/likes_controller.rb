8c4a3ce20be0707f67a75e0f897f9b280b835c91
haya <haya@appurunoMacBook-puro.local>
2020-05-23 14:09:08 +0900

    [fix]Ajaxが効かないエラーの修正(未解決)

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,20 +2,14 @@
 
 class LikesController < ApplicationController
   def create
-    # @folder = Folder.find(params[folder_id])
     @like = current_user.likes.build(folder_id: params[:folder_id])
     @like.save
-    # binding.pry
     redirect_back(fallback_location: root_path)
-    # rdirect_to root_path
   end
 
   def destroy
-    # @folder = Folder.find(params[folder_id])
     @like = Like.find_by(folder_id: params[:folder_id], user_id: current_user.id)
-    # binding.pry
     @like.destroy
     redirect_back(fallback_location: root_path)
-    # ridirect_to root_path
   end
 end
--- spec/models/folder_spec.rb
+++ spec/models/folder_spec.rb
@@ -12,20 +12,20 @@ RSpec.feature 'フォルダが表示機能' do
     # FactoryBot.create(:folder, title: 'フォルダ', user: user_a)
   end
 
-  # scenario '@userが作成したフォルダが表示される' do
-  #   # トップページを開く
-  #   visit new_user_session_path
-  #   expect(page).to have_content 'ログイン'
-  #   # # ログインフォームにEmailとパスワードを入力する
+  scenario '@userが作成したフォルダが表示される' do
+    # トップページを開く
+    visit new_user_session_path
+    expect(page).to have_content 'ログイン'
+    # # ログインフォームにEmailとパスワードを入力する
     
-  #   fill_in 'session_email', with: 'foo@example.com'
-  #   fill_in 'session_password', with: '123456'
-  #   # # # ログインボタンをクリックする
-  #   click_on 'ログイン'
-  #   # # # ログインに成功したことを検証する
-  #   expect(page).to have_content 'ログインしました'
-  #   expect(page).to have_content 'フォルダ'
-  # end
+    fill_in 'session_email', with: 'foo@example.com'
+    fill_in 'session_password', with: '123456'
+    # # # ログインボタンをクリックする
+    click_on 'ログイン'
+    # # # ログインに成功したことを検証する
+    expect(page).to have_content 'ログインしました'
+    expect(page).to have_content 'フォルダ'
+  end
 
   it 'titleが存在しない投稿を許可しない' do
     @folder.title = nil
