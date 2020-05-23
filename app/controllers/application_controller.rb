2266136c9e27d67a1509a6d60c7a155b432ccbff
haya <haya@appurunoMacBook-puro.local>
2020-05-23 16:19:25 +0900

    [fix]エラーメッセージの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  
   protected
 
   def configure_permitted_parameters
@@ -14,35 +15,17 @@ class ApplicationController < ActionController::Base
 
   private
 
-  # def logged_in_user
-  #   unless user_signed_in?
-  #     flash[:danger] = "ログインしてください。"
-  #     redirect_to new_user_session_path
-  #   end
-  # end
-
-  # def current_user?
-  #   unless authenticate_user!
-  #     flash[:danger] = "ログインしてください。"
-  #     redirect_to new_user_session_path
-  #   end
-  # end
-
   def correct_user
-    # @folder = Folder.find(params[:folder_id]) || Folder.find(params[:id])
     @folder = if params[:folder_id]
                 Folder.find(params[:folder_id])
               else
                 Folder.find(params[:id])
               end
-    # @folder = Folder.find(params[:id])
     unless current_user == @folder.user
       flash[:danger] = 'ユーザー本人のみが、行える操作です。'
       redirect_to folders_path
     end
   end
-
-  # before_action ：authenticate_user！
 
   def logged_in_user
     unless user_signed_in?
@@ -55,10 +38,7 @@ class ApplicationController < ActionController::Base
     redirect_to folders_path if user_signed_in?
   end
 
-  # def current_user?
-  #   if current_user.blank?
-  #     flash[:danger] = "ログインしてください。"
-  #     redirect_to new_user_session_path
-  #   end
-  # end
+  def record_not_found
+    render plain: "404 Not Found", status: 404
+  end
 end
