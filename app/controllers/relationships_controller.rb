549d6d565c3d589ce02783fbf02f484a59351bd9
haya <haya@appurunoMacBook-puro.local>
2020-05-23 23:43:07 +0900

    [add]フォロー機能のAjax化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,17 @@
 class RelationshipsController < ApplicationController
   def create
     @follow = current_user.active_relationships.build(follower_id: params[:user_id])
+    @user = User.find(params[:user_id])
+    # binding.pry
     @follow.save
-    redirect_back(fallback_location: user_relationships_path)
+    # redirect_back(fallback_location: user_relationships_path)
   end
 
   def destroy
     @follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
+    @user = User.find(params[:user_id])
+    # binding.pry
     @follow.destroy
-    redirect_back(fallback_location: user_relationships_path)
+    # redirect_back(fallback_location: user_relationships_path)
   end
 end
